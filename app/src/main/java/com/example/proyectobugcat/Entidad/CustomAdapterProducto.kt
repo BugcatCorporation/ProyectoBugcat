c9566accfe4c3061ea8c9d1ad2a9aa00e2455dab
MadAlexO <aluyo_2012@hotmail.com>
2023-12-01 11:29:12 -0500

    ewe

--- app/src/main/java/com/example/proyectobugcat/Entidad/CustomAdapterProducto.kt
+++ app/src/main/java/com/example/proyectobugcat/Entidad/CustomAdapterProducto.kt
@@ -1,49 +1,66 @@
+package com.example.proyectobugcat
+
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.BaseAdapter
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.recyclerview.widget.RecyclerView
 import com.example.proyectobugcat.Entidad.Producto
 import com.example.proyectobugcat.R
 import com.squareup.picasso.Picasso
 
-class CustomAdapterProducto(private val context: Context, private var productos: List<Producto>) :
-    RecyclerView.Adapter<CustomAdapterProducto.ProductoViewHolder>() {
+class CustomAdapterProducto(private val context: Context, private var productos: List<Producto>) : BaseAdapter() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductoViewHolder {
-        val view =
-            LayoutInflater.from(parent.context).inflate(R.layout.item_productos, parent, false)
-        return ProductoViewHolder(view)
-    }
-
-    override fun onBindViewHolder(holder: ProductoViewHolder, position: Int) {
-        val producto = productos[position]
-        holder.bind(producto)
-    }
-
-    override fun getItemCount(): Int {
+    override fun getCount(): Int {
         return productos.size
     }
+
+    override fun getItem(position: Int): Any {
+        return productos[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val view: View = convertView ?: LayoutInflater.from(context).inflate(R.layout.item_productos, parent, false)
+
+        val nombreTextView: TextView = view.findViewById(R.id.nombre_producto)
+        val descripcionTextView: TextView = view.findViewById(R.id.descripcion_producto)
+        val precioTextView: TextView = view.findViewById(R.id.precio_producto)
+        val imagenImageView: ImageView = view.findViewById(R.id.img_producto)
+
+        // Modificación aquí para manejar txt_id_producto
+        val idTextView: TextView? = view.findViewById(R.id.txt_id_producto)
+
+        val producto = getItem(position) as Producto
+
+        nombreTextView.text = producto.nombre
+        descripcionTextView.text = producto.descripcion
+        precioTextView.text = "S/.${producto.precio}"
+        idTextView?.text = "ID: ${producto.id}"
+
+        Picasso.get().load(producto.imagen).into(imagenImageView)
+
+        view.setOnClickListener {
+            val intent = Intent(context, EditarProducto::class.java)
+            intent.putExtra("producto", producto)
+            intent.putExtra("productoId", producto.id) // Pasa la ID
+            context.startActivity(intent)
+        }
+
+        return view
+    }
+
 
     fun actualizarProductos(nuevaLista: List<Producto>) {
         productos = nuevaLista
         notifyDataSetChanged()
     }
 
-    inner class ProductoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        private val nombreTextView: TextView = itemView.findViewById(R.id.nombre_producto)
-        private val descripcionTextView: TextView = itemView.findViewById(R.id.descripcion_producto)
-        private val precioTextView: TextView = itemView.findViewById(R.id.precio_producto)
-        private val imagenImageView: ImageView = itemView.findViewById(R.id.img_producto)
 
-        fun bind(producto: Producto) {
-            nombreTextView.text = producto.nombre
-            descripcionTextView.text = producto.descripcion
-            precioTextView.text = "S/.${producto.precio}"
-
-            Picasso.get().load(producto.imagen).into(imagenImageView)
-        }
-    }
 }
--- app/src/main/java/com/example/proyectobugcat/MantenimientoProducto.kt
+++ app/src/main/java/com/example/proyectobugcat/MantenimientoProducto.kt
@@ -1,25 +1,46 @@
 package com.example.proyectobugcat
 
+
+import android.app.Dialog
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.View
+import android.view.Window
+import android.widget.Button
 import android.widget.ListView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.proyectobugcat.Entidad.Producto
 import com.google.firebase.firestore.FirebaseFirestore
 
 class MantenimientoProducto : AppCompatActivity() {
 
-    private lateinit var productoAdapter: ProductoMantenimientoAdapter
+    private lateinit var productoAdapter: CustomAdapterProducto
     private lateinit var listViewProductos: ListView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mantenimiento_producto)
 
-        productoAdapter = ProductoMantenimientoAdapter(this, emptyList())
+        productoAdapter = CustomAdapterProducto(this, emptyList())
         listViewProductos = findViewById(R.id.mant_rvListProductos)
         listViewProductos.adapter = productoAdapter
+
+        val btnCerrarSesion = findViewById<Button>(R.id.mantp_btnCerrarSesion)
+        val btnAtras: Button = findViewById(R.id.List_btnAtras)
+
+        btnCerrarSesion.setOnClickListener{
+            val titleMsg:String = "Confirmacion"
+            val bodyMsg:String = "¿Estas seguro que desea Cerrar Sesion?"
+            showModalConfirmExit(titleMsg,bodyMsg);
+        }
+        btnAtras.setOnClickListener{
+            val mantenimientoScreen = Intent(this,MantenimientoActivity::class.java)
+            startActivity(mantenimientoScreen)
+        }
 
         cargarProductosDesdeFirestore()
     }
@@ -55,6 +76,33 @@ class MantenimientoProducto : AppCompatActivity() {
     }
 
 
+    private fun showModalConfirmExit(titleMsg: String, bodyMsg: String) {
+        val dialogConfirm = Dialog(this)
+        dialogConfirm.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialogConfirm.setCancelable(false)
+        dialogConfirm.setContentView(R.layout.custom_modal_dialog)
+        dialogConfirm.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        //Inicio
+
+        val titulo: TextView = dialogConfirm.findViewById(R.id.modalTittle)
+        val mensaje: TextView = dialogConfirm.findViewById(R.id.modalMessage)
+        val btnAceptar:Button = dialogConfirm.findViewById(R.id.btnModalAceptar)
+        val btnCancelar:Button = dialogConfirm.findViewById(R.id.btnModalCancelar)
+
+        titulo.text=titleMsg
+        mensaje.text=bodyMsg
+
+        btnAceptar.setOnClickListener{
+            val PantallaScreen = Intent(this,PantallaPrincipal::class.java)
+            startActivity(PantallaScreen)
+        }
+        btnCancelar.setOnClickListener{
+            Toast.makeText(this,"Accion cancelada", Toast.LENGTH_SHORT).show()
+            dialogConfirm.dismiss()
+        }
+        dialogConfirm.show()
+    }
 
 
 }
